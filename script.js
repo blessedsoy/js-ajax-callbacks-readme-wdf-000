5d71206409150864e5660a16a73ff163cca02091
blessedsoy <unmigong@gmail.com>
2016-08-22 17:44:06 -0400

    Done.

--- script.js
+++ script.js
@@ -6,5 +6,12 @@ $(document).ready(function(){
     // Here we are getting the element on the page with the id of sentences and
     // insert the response
     $("#sentences").html(response);
+  });ㅋ
+  $.get("this_doesnt_exist.html", function(data) {
+  // This will not be called because the .html file request doesn't exist
+    doSomethingGood();
+  }).fail(function(error) {
+    // This is called when an error occurs
+    console.log('Something went wrong: ' + error);
   });
 });
